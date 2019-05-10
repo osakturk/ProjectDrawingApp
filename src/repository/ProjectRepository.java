c6198405c3ec407328eb68e8547e9838171b2445
Ömer Aktürk <oakturk@iperasolutions.com>
2019-05-10 14:10:43 +0300

    feat: add methods for repositories

--- src/repository/ProjectRepository.java
+++ src/repository/ProjectRepository.java
@@ -1,33 +1,99 @@
 package repository;
 
 import model.Project;
+import model.Sensor;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProjectRepository {
+public class ProjectRepository extends BaseRepository{
     public List<Project> getProjectList(){
-
-        return new ArrayList<>();
+        List<Project> projectList = new ArrayList<>();
+        ArrayList<Object> parameters = new ArrayList<>();
+        this.setQuery("select * from Projects where status = ?");
+        parameters.add(1);
+        this.runQuery(parameters);
+        try {
+            while (this.resultSet.next()){
+                Project project = new Project();
+                project.setId(this.resultSet.getLong("id"));
+                project.setName(this.resultSet.getString("name"));
+                project.setCreateDate(this.resultSet.getDate("created_time"));
+                project.setPlanImage(this.resultSet.getBytes("plan_image"));
+                projectList.add(project);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+            e.getSQLState();
+        }finally {
+            this.cleanResources();
+        }
+        return projectList;
     }
 
     public Project getSpecificProject(Long id){
-
-        return new Project();
+        Project project = new Project();
+        ArrayList<Object> parameters = new ArrayList<>();
+        this.setQuery("select * from Projects where status = ? and id = ?");
+        parameters.add(1);
+        parameters.add(id);
+        this.runQuery(parameters);
+        try {
+            if (this.resultSet.next()){
+                project.setId(this.resultSet.getLong("id"));
+                project.setName(this.resultSet.getString("name"));
+                project.setCreateDate(this.resultSet.getDate("created_time"));
+                project.setPlanImage(this.resultSet.getBytes("plan_image"));
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally {
+            this.cleanResources();
+        }
+        parameters = new ArrayList<>();
+        this.setQuery("select * from Sensors where project_id = ? ");
+        parameters.add(id);
+        this.runQuery(parameters);
+        List<Sensor> sensorList = new ArrayList<>();
+        try {
+            while (this.resultSet.next()){
+                Sensor sensor = new Sensor();
+                sensor.setId(this.resultSet.getLong("id"));
+                sensor.setLabel(this.resultSet.getString("name"));
+                sensor.setLevel(this.resultSet.getString("level"));
+                sensor.setxLocation(this.resultSet.getDouble("x_location"));
+                sensor.setyLocation(this.resultSet.getDouble("y_location"));
+                sensorList.add(sensor);
+            }
+            project.setSensorList(sensorList);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally {
+            this.cleanResources();
+        }
+        return project;
     }
 
-    public boolean deleteProject(Project project){
-
-        return true;
+    public boolean deleteProject(Long id){
+        ArrayList<Object> parameters = new ArrayList<>();
+        this.setQuery("update Project set status = 0 where project_id = ?");
+        parameters.add(id);
+        this.runQuery(parameters);
+        return this.resultForExecuteUpdate == 1;
     }
 
     public boolean addNewProject(Project project){
-
+        ArrayList<Object> parameters = new ArrayList<>();
+        this.setQuery("");
+        this.runQuery(parameters);
         return true;
     }
 
     public boolean updateProject(Project project){
-
-        return true;
+        ArrayList<Object> parameters = new ArrayList<>();
+        this.setQuery("update Project set ???? where project_id = ?");
+        this.runQuery(parameters);
+        return this.resultForExecuteUpdate == 1;
     }
 }
--- src/repository/SensorRepository.java
+++ src/repository/SensorRepository.java
@@ -1,33 +1,86 @@
 package repository;
 
+import model.Project;
 import model.Sensor;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SensorRepository {
+public class SensorRepository extends BaseRepository {
     public List<Sensor> getSensorListByProjectId(Long projectId){
-
-        return new ArrayList<>();
+        List<Sensor>     sensorList = new ArrayList<>();
+        ArrayList<Object> parameters  = new ArrayList<>();
+        this.setQuery("select * from Sensors where status = ?");
+        parameters.add(1);
+        this.runQuery(parameters);
+        try {
+            while (this.resultSet.next()){
+                Sensor sensor = new Sensor();
+                sensor.setId(this.resultSet.getLong("id"));
+                sensor.setLabel(this.resultSet.getString("name"));
+                sensor.setLabel(this.resultSet.getString("description"));
+                sensor.setxLocation(this.resultSet.getDouble("x_location"));
+                sensor.setyLocation(this.resultSet.getDouble("y_location"));
+                sensor.setType(this.resultSet.getInt("type"));
+                sensor.setProjectId(this.resultSet.getLong("project_id"));
+                sensorList.add(sensor);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+            e.getSQLState();
+        }finally {
+            this.cleanResources();
+        }
+        return sensorList;
     }
 
-    public Sensor getSpecificSensorById(Long SensorId){
+    public Sensor getSpecificSensorById(Long id){
 
-        return new Sensor();
+        Sensor sensor = new Sensor();
+        ArrayList<Object> parameters = new ArrayList<>();
+        this.setQuery("select * from Sensors where status = ? and id = ?");
+        parameters.add(1);
+        parameters.add(id);
+        this.runQuery(parameters);
+        try {
+            if (this.resultSet.next()){
+                sensor.setId(this.resultSet.getLong("id"));
+                sensor.setLabel(this.resultSet.getString("name"));
+                sensor.setLabel(this.resultSet.getString("description"));
+                sensor.setxLocation(this.resultSet.getDouble("x_location"));
+                sensor.setyLocation(this.resultSet.getDouble("y_location"));
+                sensor.setType(this.resultSet.getInt("type"));
+                sensor.setProjectId(this.resultSet.getLong("project_id"));
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally {
+            this.cleanResources();
+        }
+        return sensor;
     }
 
-    public boolean deleteSensor(Sensor sensor){
-
-        return true;
+    public boolean deleteSensor(Long sensorId, Long projectId){
+        ArrayList<Object> parameters = new ArrayList<>();
+        this.setQuery("update Sensors set status = 0 where project_id = ? and id = ?");
+        parameters.add(projectId);
+        parameters.add(sensorId);
+        this.runQuery(parameters);
+        return this.resultForExecuteUpdate == 1;
     }
 
     public boolean addNewSensor(Sensor sensor){
-
+        ArrayList<Object> parameters = new ArrayList<>();
+        this.setQuery("");
+        this.runQuery(parameters);
         return true;
     }
 
     public boolean updateSensor(Sensor sensor){
-
-        return true;
+        ArrayList<Object> parameters = new ArrayList<>();
+        this.setQuery("update Sensors set ???? where project_id = ?");
+        this.runQuery(parameters);
+        return this.resultForExecuteUpdate == 1;
     }
 }
