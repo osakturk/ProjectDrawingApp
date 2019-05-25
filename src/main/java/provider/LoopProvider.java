c1c9b0674b1ad3e272c5cd04e17fa2a17923e580
Omer <omerakturk@outlook.com.tr>
2019-05-26 00:48:23 +0300

    fix: delete method from loop provider

--- src/main/java/provider/LoopProvider.java
+++ src/main/java/provider/LoopProvider.java
@@ -1,14 +1,4 @@
 package provider;
 
-import model.Loop;
-import repository.LoopRepository;
-
-import java.util.List;
-
 public class LoopProvider {
-
-    public List<Loop> getLoops() {
-        LoopRepository loopRepository = new LoopRepository();
-        return loopRepository.getLoopList();
-    }
 }
