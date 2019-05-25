08d1eb289949dbe98da37865be765e2cb2fed55b
Omer <omerakturk@outlook.com.tr>
2019-05-26 00:48:11 +0300

    perf: change method for performance

--- src/main/java/repository/LoopRepository.java
+++ src/main/java/repository/LoopRepository.java
@@ -5,47 +5,37 @@ import model.Point;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import provider.XmlProvider;
-import provider.ZoneProvider;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class LoopRepository {
+class LoopRepository {
 
-    public List<Loop> getLoopList() {
-        ZoneProvider zoneProvider = new ZoneProvider();
+    List<Loop> getLoopList(NodeList pointList, NodeList zoneList) {
+        ZoneRepository zoneRepository = new ZoneRepository();
         List<Loop> loops = new ArrayList<>();
-        //        NodeList nodeList = doc.getElementsByTagName("point");
-        NodeList nodeList = XmlProvider.getNodeList("LOCATION_OF_XML_FILE", "point");
-        for (int i = 0; i < nodeList.getLength(); i++) {
-
-            Node nNode = nodeList.item(i);
-
-            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                Loop loop = new Loop();
-                List<Point> points = new ArrayList<>();
-                Point point = new Point();
-                Element eElement = (Element) nNode;
-                point.setAddress(eElement.getAttribute("no"));
-                point.setLabel(eElement.getAttribute("lang1"));
-                point.setLoop(getCharForNumber(Integer.parseInt(eElement.getAttribute("address"))));
-                point.setDeviceType(eElement.getElementsByTagName("devtype").item(0).getTextContent());
-                point.setZoneInfo(zoneProvider.getZoneInfoById(eElement.getElementsByTagName("zone").item(0).getTextContent()).getZoneInfo());
+        for (int j = 0; j < pointList.getLength(); j++) {
+            Loop loop = new Loop();
+            List<Point> points = new ArrayList<>();
+            Point point = new Point();
+            Node node = pointList.item(j);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element element1 = (Element) node;
+                point.setAddress(element1.getAttribute("no"));
+                point.setLabel(element1.getAttribute("lang1"));
+                point.setLoop("LOOP" + element1.getAttribute("address"));
+                point.setDeviceType(element1.getElementsByTagName("devtype").item(0).getTextContent());
+                point.setZoneInfo(zoneRepository.getZoneInfoById(zoneList, element1.getElementsByTagName("zone").item(0).getTextContent()).getZoneInfo());
                 points.add(point);
-                loop.setLoopNumber(eElement.getAttribute("address"));
-                loop.setLoopName(getCharForNumber(Integer.parseInt(eElement.getAttribute("address"))));
+                loop.setLoopNumber(element1.getAttribute("address"));
                 loop.setPoints(points);
-                loops.add(loop);
+                if (Integer.parseInt(element1.getAttribute("category")) == 0
+                        && Integer.parseInt(element1.getElementsByTagName("zone").item(0).getTextContent()) != 0) {
+                    loops.add(loop);
+                }
             }
         }
-
-
         return loops;
-    }
-
-    private String getCharForNumber(int i) {
-        return i > -1 && i < 26 ? String.valueOf((char) (i + 65)) : null;
     }
 }
 
