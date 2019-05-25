fa681ca7bb9035bb76462607d60b99f6bfeb1bc3
Omer <omerakturk@outlook.com.tr>
2019-05-26 00:47:21 +0300

    fix: delete zone list method from zone provider

--- src/main/java/provider/ZoneProvider.java
+++ src/main/java/provider/ZoneProvider.java
@@ -1,11 +1,5 @@
 package provider;
 
-import model.Zone;
-import repository.ZoneRepository;
-
 public class ZoneProvider {
-    public Zone getZoneInfoById(String zoneId) {
-        ZoneRepository zoneRepository = new ZoneRepository();
-        return zoneRepository.getZoneInfoById(zoneId);
-    }
+
 }
