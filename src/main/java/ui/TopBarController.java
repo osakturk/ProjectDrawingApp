1c992bd3555f9ada1ba07220b08cff0b5a4346f1
JesterPC <ahmetkanbur@gmail.com>
2019-05-28 00:48:49 +0300

    feat: tooltip test

--- src/main/java/ui/TopBarController.java
+++ src/main/java/ui/TopBarController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,11 +16,19 @@ public class TopBarController implements Initializable {
     @FXML private Button uiNewProjectBtn;
     @FXML private Button uiSettingsBtn;
 
+    @FXML private Button uiBackBtn;
+    @FXML private Button uiForwardBtn;
+    @FXML private Button uiHomeBtn;
+    @FXML private Button uiZoomInBtn;
+    @FXML private Button uiZoomOutBtn;
+
+
     @Override
     public void initialize(URL url, ResourceBundle rb ){
 
 
-
+        Tooltip tt = new Tooltip("Test");
+        uiBackBtn.setTooltip(tt);
 
 
     }
