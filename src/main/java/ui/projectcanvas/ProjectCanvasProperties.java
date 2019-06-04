5f582a449475330cbce014f035f5cb9fb8824af7
JesterPC <ahmetkanbur@gmail.com>
2019-06-04 11:27:39 +0300

    feat: added layout and methods for PCEProjectCanvasProperties

--- src/main/java/ui/projectcanvas/ProjectCanvasProperties.java
+++ src/main/java/ui/projectcanvas/ProjectCanvasProperties.java
@@ -1,4 +1,16 @@
 package ui.projectcanvas;
 
-public class ProjectCanvasProperties {
+import model.Project;
+import ui.UIComponent;
+
+public class ProjectCanvasProperties extends UIComponent {
+
+    public ProjectCanvasProperties(){
+        loadFXML("pce_project_canvas_properties");
+    }
+
+    public ProjectCanvasProperties( Project project ){
+        loadFXML("pce_project_canvas_properties");
+    }
+
 }
--- src/main/java/ui/projectcanvas/ProjectCanvasPropertiesController.java
+++ src/main/java/ui/projectcanvas/ProjectCanvasPropertiesController.java
@@ -1,4 +1,22 @@
 package ui.projectcanvas;
 
-public class ProjectCanvasPropertiesController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ProjectCanvasPropertiesController implements Initializable {
+
+    @FXML private Label uiNameLabel;
+    @FXML private TextField uiNameInput;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+
+    }
 }
