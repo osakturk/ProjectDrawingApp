b29756847a3fe8746ed4c1fddf7881b094bd34eb
JesterPC <ahmetkanbur@gmail.com>
2019-06-04 11:10:43 +0300

    feat: added layout and methods for PCEZoneProperties

--- src/main/java/ui/projectcanvas/PCEZoneProperties.java
+++ src/main/java/ui/projectcanvas/PCEZoneProperties.java
@@ -1,4 +1,18 @@
 package ui.projectcanvas;
 
-public class PCEZoneProperties {
+import model.Zone;
+import ui.UIComponent;
+
+public class PCEZoneProperties extends UIComponent {
+
+
+    public PCEZoneProperties(){
+        loadFXML("pce_zone_properties");
+    }
+
+    public PCEZoneProperties( Zone zone ){
+        loadFXML("pce_zone_properties");
+        ((PCEZonePropertiesController) controller).fillForm( zone ); // @todo parameters
+    }
+
 }
--- src/main/java/ui/projectcanvas/PCEZonePropertiesController.java
+++ src/main/java/ui/projectcanvas/PCEZonePropertiesController.java
@@ -1,4 +1,31 @@
 package ui.projectcanvas;
 
-public class PCEZonePropertiesController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.Zone;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PCEZonePropertiesController implements Initializable{
+
+    @FXML private Label uiNameLabel;
+    @FXML private TextField uiNameInput;
+    @FXML private Button uiXMLDataBtn;
+    @FXML private Label uiXMLDataLabel;
+    @FXML private Button uiLinkBtn;
+    @FXML private Label uiLinkLabel;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void fillForm( Zone zone ){
+
+    }
+
 }
