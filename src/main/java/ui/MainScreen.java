29a70e4e2b21b3cdc2ac020e720fc8bbbfcdeb89
Omer <omerakturk@outlook.com.tr>
2019-06-16 13:26:00 +0300

    feat(MainScreen): add new listener for data

--- src/main/java/ui/MainScreen.java
+++ src/main/java/ui/MainScreen.java
@@ -1,10 +1,17 @@
 package ui;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainScreen extends Application {
 
@@ -17,7 +24,17 @@ public class MainScreen extends Application {
             primaryStage.setTitle("Project Drawing App");
 
 
+            Timeline timeline = new Timeline(new KeyFrame(
+                    Duration.millis(2500),
+                    ae -> System.out.println("CUF CUF")));
+            timeline.setCycleCount(Animation.INDEFINITE);
+            timeline.play();
 
+            /** we should define new properties for live sensor data
+             * you can get all sensor data from here
+             *  we can set some sensor for live data
+             *  you can listen from there
+             * */
             //Map<String, Double> resData = Common.calculateAppWindowSize();
             primaryStage.setScene(new Scene(content, 1280, 1024 )); // @todo - calculate client's width-height, give offset to that
 
